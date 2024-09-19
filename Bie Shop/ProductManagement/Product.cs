ba5cc15dd4cba840af59358d75b9d90490f13b2e
Salah Tamer <ma9060139@gmail.com>
2024-09-20 02:55:25 +0300

    Add project files.

---
+++ Bie Shop/ProductManagement/Product.cs
@@ -0,0 +1,137 @@
+﻿using Bie_Shop.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bie_Shop.ProductManagement
+{
+    public partial class Product
+    {
+        private int id;
+        private string name;
+        private string? description; //nullable
+        private int maxItemsInStock = 0;
+
+        public Product(int id, string name)
+        {
+            Name = name;
+            Id = id;
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value.Length > 50 ? value[..50] : value;
+            }
+        }
+
+        public string? Description
+        {
+            get
+            {
+                return description;
+            }
+
+            set
+            {
+                if (value == null)
+                {
+                    description = string.Empty;
+                }
+                else
+                {
+                    description = value.Length > 250 ? value[..250] : value;
+                }
+            }
+        }
+
+        public UnitType UnitType { get; set; }
+        public int AmountInStock { get; private set; }
+        public bool IsBelowStockThreshold { get; private set; }
+        public Price Price { get; set; }
+
+        public void UseProduct(int items)
+        {
+            if (items <= AmountInStock)
+            {
+                AmountInStock -= items;
+                UpdateLowStock();
+                Log($"Amount in Stock is Updated. Now {AmountInStock} items in stock.");
+            }
+
+            else
+            {
+                Log($"Not enough items on stock. {AmountInStock} available but {items} requested.");
+            }
+        }
+
+        public void IncreaseStock()
+        {
+            AmountInStock++;
+        }
+
+        public void IncreaseStock(int amount)
+        {
+            int newStock = amount + AmountInStock;
+            if (newStock <= maxItemsInStock)
+            {
+                AmountInStock += amount;
+            }
+            else
+            {
+                AmountInStock = maxItemsInStock;
+            }
+
+            if (AmountInStock > 10)
+            {
+                IsBelowStockThreshold = false;
+            }
+
+        }
+
+        public Product(int id, string name, string? description, Price price, UnitType unitType, int maxAmountInStock)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Price = price;
+            UnitType = unitType;
+
+            maxItemsInStock = maxAmountInStock;
+
+            UpdateLowStock();
+        }
+
+
+        public string DisplayDetailsShort()
+        {
+            return $"{id} {name} \n{AmountInStock} items in stock";
+        }
+        public string DisplayDetailsFull()
+        {
+            StringBuilder sb = new();
+            sb.AppendLine($"{id} {name} \n{description}\n{AmountInStock} item(s) in stock");
+
+            if (IsBelowStockThreshold)
+            {
+                sb.AppendLine("!! STOCK LOW !!");
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ Bie Shop/ProductRepository.cs
@@ -0,0 +1,109 @@
+﻿using Bie_Shop.General;
+using Bie_Shop.ProductManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bie_Shop
+{
+    internal class ProductRepository
+    {
+        string filePath = "products.txt";
+
+        private void checkForExistingProductFile()
+        {
+            bool ExistingFileFound = File.Exists(filePath);
+            if(!ExistingFileFound)
+            {
+               using FileStream fs = File.Create(filePath);
+            }
+        }
+
+        public List<Product> LoadProductsFromFile()
+        {
+            List<Product> products = new List<Product>();
+            try
+            {
+                checkForExistingProductFile();
+
+                string[] productsAsString = File.ReadAllLines(filePath);
+                for (int i = 0; i < productsAsString.Length; i++)
+                {
+                    string[] productSplits = productsAsString[i].Split(';');
+
+                    bool success = int.TryParse(productSplits[0], out int productId);
+                    if (!success)
+                    {
+                        productId = 0; //defaul value
+                    }
+
+                    string name = productSplits[1];
+                    string description = productSplits[2];
+
+                    success = int.TryParse(productSplits[3], out int maxItemsInStock);
+                    if (!success)
+                    {
+                        maxItemsInStock = 100; //default value
+                    }
+
+                    success = int.TryParse(productSplits[4], out int ItemPrice);
+                    if (!success)
+                    {
+                        ItemPrice = 0; //default value
+                    }
+
+                    success = Enum.TryParse(productSplits[5], out Currency currency);
+                    if (!success)
+                    {
+                        currency = Currency.Dollar; //default value
+                    }
+
+                    success = Enum.TryParse(productSplits[6], out UnitType unitType);
+                    if (!success)
+                    {
+                        unitType = UnitType.PerItem; //default value
+                    }
+
+                    Product product = new Product
+                    (
+                        productId,
+                        name,
+                        description,
+                        new Price() { Currency = currency, itemPrice = ItemPrice },
+                        unitType,
+                        maxItemsInStock
+                    );
+
+                    products.Add(product);
+                }
+            }
+
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Something went wrong while parsing the file, please check the data!");
+                Console.WriteLine(ex.Message);
+            }
+            catch (FileNotFoundException fnfx)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("The file couldn't be found");
+                Console.WriteLine(fnfx.Message);
+            }
+            catch (Exception iex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Something went wrong while loading the file!");
+                Console.WriteLine(iex.Message);
+            }
+            finally 
+            {
+                Console.ResetColor();
+            }
+
+            return products;
+        }
+    }
+}
